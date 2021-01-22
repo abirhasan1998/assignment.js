ca620d80a2440c2411aa45db4bf8a0b6dd2a21a2
UNMAD_ABIR <abirhasan3067@gmail.com>
2021-01-22 21:29:47 +0600

    test push

--- megaFriend.js
+++ megaFriend.js
@@ -10,7 +10,7 @@ function megaFriend(friends){
        maximum= friends[0].length;
        longest=friends[0];
        for(j=0; j<friends.length; j++ ){
-           if(friends[j].length<maximum){
+           if(friends[j].length>maximum){
                maximum=friends[j].length;
                longest=friends[j];
 
@@ -27,3 +27,67 @@ function megaFriend(friends){
 var arrayName=["Abir","Kawsar","Jubayer","Rashidul","Mehidi"];
 var result = megaFriend(arrayName);
 console.log(result);
+
+
+
+
+
+// function kilometerToMeter(kilometer){
+//     var meter;
+//     if(kilometer > 0){
+//         meter = kilometer * 1000;
+//         return meter;
+//     }
+//     else{
+//         return "Invalid";
+//     }
+    
+// }
+
+// function budgetCalculator(qWatch, qMobile, qLaptop){
+//     var watchCost = 50 * qWatch;
+//     var mobileCost = 100 * qMobile;
+//     var laptopCost = 500 * qLaptop;
+    
+//     var total = watchCost + mobileCost + laptopCost;
+//     return total;
+// }
+
+// function hotelCost(days){
+//     var cost;
+//     if(days > 0){
+//         if(days <= 10){
+//             cost = 100 * days;
+//             return cost;
+//         }
+//         else if(days <= 20){
+//             var firstpart = 10 * 100;
+//             var remain = days - 10;
+//             var secondpart = 80 * remain;
+//             cost = firstpart + secondpart;
+//             return cost;
+//         }
+//         else{
+//             var firstpart = 10 * 100;
+//             var secondpart = 10 * 80;
+//             var remain = days - 20;
+//             var thirdpart = 50 * remain;
+//             cost = firstpart + secondpart + thirdpart;
+//             return cost;
+//         }
+//     }
+//     else{
+//         return "Days must be positive";
+//     }
+// }
+
+
+// function megaFriend(friends){
+//     var large = friends[0];
+//     for(var i = 0; i < friends.length; i++){
+//         if(friends[i].length > large.length){
+//             large = friends[i];
+//         }
+//     }
+//     return large ;
+// }
